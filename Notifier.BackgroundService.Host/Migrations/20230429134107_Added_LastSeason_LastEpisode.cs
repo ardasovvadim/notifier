9276db0d8945f03c73fb037a26d3c6fa56fb81cb
ardasovvadim <ardasovvadim@gmail.co>
2023-04-29 17:26:35 +0300

    # added migration

---
+++ Notifier.BackgroundService.Host/Migrations/20230429134107_Added_LastSeason_LastEpisode.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Notifier.BackgroundService.Host.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_LastSeason_LastEpisode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LastEpisode",
+                table: "MoviesRecords",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LastSeason",
+                table: "MoviesRecords",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastEpisode",
+                table: "MoviesRecords");
+
+            migrationBuilder.DropColumn(
+                name: "LastSeason",
+                table: "MoviesRecords");
+        }
+    }
+}
